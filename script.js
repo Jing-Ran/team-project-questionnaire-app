60b17a33f7ff4e4f8b6701c4d6491d4765dfbe61
Vojislav Grujic <vojislav.grujic@gmail.com>
2016-06-26 11:14:17 +0200

    Change code formatting in script.js file.

--- script.js
+++ script.js
@@ -1,52 +1,56 @@
 (function() {
-var questionnaire = document.getElementById('questionnaire');
+  var questionnaire = document.getElementById('questionnaire');
 
-function trimSpaces(string) {
-  return string.replace(/^\s+/, '').replace(/\s+$/, '');
-}
+  function trimSpaces(string) {
+    return string.replace(/^\s+/, '').replace(/\s+$/, '');
+  }
 
-function getInfo(e) {
-	e.preventDefault();
-  this.data = {};
-  var appTitle = this.querySelector('h1.title').textContent,
-  		cardElements = this.getElementsByClassName("questionnaire-card"),
-      cardsNum = cardElements.length;
-      
-  this.data.appTitle = trimSpaces(appTitle);
-  this.data.appDescription = trimSpaces(appTitle);
-  this.data.cards = [];
+  function getInfo(e) {
+    e.preventDefault();
+    this.data = {};
+    var appTitle = this.querySelector('h1.title').textContent,
+        cardElements = this.getElementsByClassName("questionnaire-card"),
+        cardsNum = cardElements.length;
 
-	for ( var i = 0; i < cardsNum; i++ ) {
-  	if ( cardElements[i].getAttribute('disabled') ) continue;
-  	var questionElements = cardElements[i].getElementsByClassName('question');
-    var cardObj = {};
-		cardObj.title = trimSpaces(cardElements[i].getElementsByClassName('title')[0].textContent);
-    cardObj.description = (cardElements[i].getElementsByClassName('.card-description')[0]) ? trimSpaces(cardElements[i].getElementsByClassName('.card-description')[0].textContent) : '';
-    cardObj.questions = [];
-    for (var j = 0; j < questionElements.length; j++) {
-    	var questionObj = {},
-          answers,
-          answerNum;
+    this.data.appTitle = trimSpaces(appTitle);
+    this.data.appDescription = trimSpaces(appTitle);
+    this.data.cards = [];
 
-			questionObj.qText = trimSpaces(questionElements[j].getElementsByClassName('question-text')[0].textContent);
-      questionObj.qAnswer = [];
-      answers = questionElements[j].querySelectorAll('[class*="answer"]');
-      answerNum = answers.length;
-      for ( var k = 0; k < answerNum; k++ ) {
-        if ( answers[k].selected || answers[k].checked ) {
-          questionObj.qAnswer.push(answers[k].value);
+    for ( var i = 0; i < cardsNum; i++ ) {
+      if ( cardElements[i].getAttribute('disabled') ) continue;
+      var questionElements = cardElements[i].getElementsByClassName('question');
+      var cardObj = {};
+
+      cardObj.title = trimSpaces(cardElements[i].getElementsByClassName('title')[0].textContent);
+      cardObj.description = (cardElements[i].getElementsByClassName('.card-description')[0]) ? trimSpaces(cardElements[i].getElementsByClassName('.card-description')[0].textContent) : '';
+      cardObj.questions = [];
+
+      for (var j = 0; j < questionElements.length; j++) {
+        var questionObj = {},
+            answers,
+            answerNum;
+
+        questionObj.qText = trimSpaces(questionElements[j].getElementsByClassName('question-text')[0].textContent);
+        questionObj.qAnswer = [];
+        answers = questionElements[j].querySelectorAll('[class*="answer"]');
+        answerNum = answers.length;
+
+        for ( var k = 0; k < answerNum; k++ ) {
+          if ( answers[k].selected || answers[k].checked ) {
+            questionObj.qAnswer.push(answers[k].value);
+          }
         }
-      }
-      console.log(questionObj);
-      cardObj.questions.push(questionObj);
-  	}
-    this.data.cards.push(cardObj);
-	}
-  
-  
-  console.log(JSON.stringify(questionnaire.data, null, 2));
-}
 
-questionnaire.addEventListener('submit', getInfo);
+        //console.log(questionObj);
+        cardObj.questions.push(questionObj);
+      }
+      this.data.cards.push(cardObj);
+    }
+
+
+    console.log(JSON.stringify(questionnaire.data, null, 2));
+  }
+
+  questionnaire.addEventListener('submit', getInfo);
 
 })();
