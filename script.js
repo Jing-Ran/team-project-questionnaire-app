6d3cbbcb721e8129ee82811af7d258f429fdc4e3
Jing Ran <jingran.mavis@gmail.com>
2016-07-17 23:41:55 -0400

    Move validation functions to validator lib and add contraint api

--- script.js
+++ script.js
@@ -1,90 +1,26 @@
 (function() {
   var questionnaire = document.querySelector('#questionnaire'),
-      validator = {},
-      fullName = document.getElementsByClassName("full-name")[0],
-      emailAddress = document.getElementsByClassName("email")[0];
+      fullName = document.getElementById("user-name"),
+      emailAddress = document.getElementById("user-email");
 
   function trimSpaces(string) {
     return string.replace(/^\s+/, '').replace(/\s+$/, '');
   }
 
-  // update validation functions
-  // check full-name validation:
-  validator.isFullName = function(name) {
-    var trimmedName = trimSpaces(name),
-        nameArr = name.split(" ");
-    if (nameArr.length >= 2) {
-        for (var i = 0; i < nameArr.length; i++) {
-            if (nameArr[i].length >= 2) {
-                for (var j = 0; j < nameArr[i].length; j++){
-                    if (nameArr[i].charCodeAt(j) >= 48 &&
-                        nameArr[i].charCodeAt(j) <= 57)
-                        return false;
-                }
-            } else {
-                return false;
-            }
-        }
-        return true;
+  function checkFullName(element) {
+    var value = element.value;
+    if (validator.isEmpty(value)) {
+      console.log("is empty");
+      element.setCustomValidity("Required field");
+    } else if (!validator.isFullName(value)) {
+      element.setCustomValidity("Invalid full name format");
+    } else {
+      element.setCustomValidity("");
     }
-    return false;
-  };
-
-  //modify check email validation:
-  validator.isEmail = function(email) {
-    var beforeAt,
-        afterAt,
-        splitBeforeAt,
-        splitAfterAt;
-
-    if (!email) return false;
-
-    if (email.indexOf("@") === -1 ||
-        email.indexOf("@") !== email.lastIndexOf("@")) return false;
-
-    beforeAt = email.split("@")[0];
-    afterAt = email.split("@")[1];
-    splitBeforeAt = beforeAt.split(".");
-    splitAfterAt = afterAt.split(".");
-
-    if (beforeAt === "" || beforeAt.indexOf(" ") >= 0) return false;
-    for (var i = 0; i < splitBeforeAt.length; i++) {
-        if (splitBeforeAt[i] === "" || splitBeforeAt[i] === " ")
-            return false;
-    }
-    if (afterAt === "" || afterAt.indexOf(" ") >=0) return false;
-    if (splitAfterAt.length < 2) return false;
-    for (var j = 0; j < splitAfterAt.length; j++) {
-        if (splitAfterAt[j] === "" || splitAfterAt[j] === " ")
-            return false;
-        if (splitAfterAt[splitAfterAt.length - 1].length < 2) return false;
-    }
-
-    return true;
-  };
+  }
 
   function getInfo(e) {
     e.preventDefault();
-    //Update: call validation check functions
-    if (!validator.isFullName(fullName.value) || 
-      !validator.isEmail(emailAddress.value)) {
-      if (!validator.isFullName(fullName.value)){
-        fullName.classList.add("invalid");
-      } else {
-        fullName.classList.remove("invalid");
-      }
-      if (!validator.isEmail(emailAddress.value)){
-        emailAddress.classList.add("invalid");
-      } else {
-        emailAddress.classList.remove("invalid");
-      }
-      return;
-    } else {
-      fullName.classList.remove("invalid");
-      emailAddress.classList.remove("invalid");
-    }
-
-      
 
     this.data = {};
     var appTitle = this.querySelector('h1.title').textContent,
@@ -131,6 +67,15 @@
 
     console.log(JSON.stringify(questionnaire.data, null, 2));
   }
+
+
+  fullName.addEventListener("keyup", function() {
+    checkFullName(this);
+  });
+  emailAddress.addEventListener("keyup", function() {
+    checkEmail(this);
+  });
+
   
 
   questionnaire.addEventListener('submit', getInfo);
